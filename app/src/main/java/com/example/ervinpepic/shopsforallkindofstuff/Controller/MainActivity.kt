76b676ba55bc267471db2ae28f04b59c98ca97c9
Ervin Pepic <ervin.hack@gmail.com>
2018-02-15 21:59:01 +0100

    Refacotr ListAdapter View completely to Recycle Adapter View

--- app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Controller/MainActivity.kt
+++ app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Controller/MainActivity.kt
@@ -2,22 +2,27 @@ package com.example.ervinpepic.shopsforallkindofstuff.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ervinpepic.shopsforallkindofstuff.Adapters.CategoryAdapter
+import android.support.v7.widget.LinearLayoutManager
+import com.example.ervinpepic.shopsforallkindofstuff.Adapters.CategoryRecycleAdapter
 import com.example.ervinpepic.shopsforallkindofstuff.R
 import com.example.ervinpepic.shopsforallkindofstuff.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
-
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
+
         }
     }
 
