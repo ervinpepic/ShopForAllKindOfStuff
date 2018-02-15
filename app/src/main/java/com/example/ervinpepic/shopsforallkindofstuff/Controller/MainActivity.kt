2697ed83d606f8cb6c3b6132958e75c9a16fb3d2
Ervin Pepic <ervin.hack@gmail.com>
2018-02-15 14:44:45 +0100

    Added new custom adapter to hold categories

--- app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Controller/MainActivity.kt
+++ app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ervinpepic.shopsforallkindofstuff.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.ervinpepic.shopsforallkindofstuff.Adapters.CategoryAdapter
 import com.example.ervinpepic.shopsforallkindofstuff.Model.Category
 import com.example.ervinpepic.shopsforallkindofstuff.R
 import com.example.ervinpepic.shopsforallkindofstuff.Services.DataService
@@ -10,13 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
--- app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Services/DataService.kt
+++ app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Services/DataService.kt
@@ -8,7 +8,7 @@ object DataService {
             Category("SHIRTS", "shirtimage"),
             Category("HOODIES", "hoodieimage"),
             Category("HATS", "hatimage"),
-            Category("DIGITAL", "ditigalgoodsimage")
+            Category("DIGITAL", "digitalgoodsimage")
     )
 
     val hats = listOf(
