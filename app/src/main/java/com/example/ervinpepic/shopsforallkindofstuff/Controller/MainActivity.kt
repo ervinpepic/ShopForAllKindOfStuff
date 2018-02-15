ebe0737d82f3a48d3bd9226fd3ef1427fbbc3277
Ervin Pepic <ervin.hack@gmail.com>
2018-02-15 21:37:26 +0100

    Added RecyclerView

--- app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Controller/MainActivity.kt
+++ app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Controller/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.ervinpepic.shopsforallkindofstuff.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import com.example.ervinpepic.shopsforallkindofstuff.Adapters.CategoryAdapter
-import com.example.ervinpepic.shopsforallkindofstuff.Model.Category
 import com.example.ervinpepic.shopsforallkindofstuff.R
 import com.example.ervinpepic.shopsforallkindofstuff.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,12 +18,6 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
-
-            categoryListView.setOnItemClickListener { adapterView, view, i, l ->
-                val category = DataService.categories[i]
-                Toast.makeText(this, "You clicked on the ${category.title} cell", Toast.LENGTH_SHORT).show()
-
-            }
         }
     }
 
