15a37269387ac12714287413f566d1c17e5f1841
Ervin Pepic <ervin.hack@gmail.com>
2018-02-19 16:32:25 -0600

    Added ability to list all products

--- app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/ervinpepic/shopsforallkindofstuff/Controller/ProductsActivity.kt
@@ -1,16 +1,40 @@
 package com.example.ervinpepic.shopsforallkindofstuff.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.example.ervinpepic.shopsforallkindofstuff.Adapters.ProductsAdapter
 import com.example.ervinpepic.shopsforallkindofstuff.R
+import com.example.ervinpepic.shopsforallkindofstuff.Services.DataService
 import com.example.ervinpepic.shopsforallkindofstuff.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
     }
 }
